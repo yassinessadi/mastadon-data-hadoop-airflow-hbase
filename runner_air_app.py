8d4fc90c61a43968441d8db22103c9c067a63232
yassinessadi <yasine.esadi@gmail.com>
2023-10-25 10:44:54 +0100

    airflow added.

--- runner_air_app.py
+++ runner_air_app.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'jane',
-    'start_date': datetime(2023, 10, 23),
+    'start_date': datetime(2023, 10, 24),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=3),
 }
 
 # Create an instance of the DAG
@@ -14,27 +14,28 @@ dag = DAG(
     'task_to_run',
     default_args=default_args,
     description='Data pipeline for collecting and analyzing data from Mastodon',
-    schedule_interval=timedelta(minutes=5),
+    schedule_interval=timedelta(minutes=3),
     catchup=False,
 )
 
 # Run Task to get data
 run_get_data = BashOperator(
     task_id='run_get_data_task',
-    bash_command="python3 /home/jane/mastodon/extraction_data.py",
+    bash_command="python3 ~/mastodon/extraction_data.py",
     dag=dag,
 )
 
 # Task to run the Reducer script
+
 run_mapreducer = BashOperator(
     task_id='run_mapreducer_task',
-    bash_command="python3 /home/jane/mastodon/wordcounter.py /home/jane/mastodon/sample.json > ./output.txt",
+    bash_command="python3 ~/mastodon/wordcounter.py ~/mastodon/sample.json > ~/mastodon/output.txt",
     dag=dag,
 )
 
 run_inset_hbase = BashOperator(
     task_id='insert_into_hbase',
-    bash_command='python3 /home/jane/mastodon/connection.py',
+    bash_command='python3 ~/mastodon/connection.py',
     dag=dag
 )
 
