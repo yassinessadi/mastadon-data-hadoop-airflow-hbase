0f45c626b8856a752a108a8dac9fec420c787394
yassinessadi <yasine.esadi@gmail.com>
2023-10-25 10:41:02 +0100

    get engagement and top followers

--- connection.py
+++ connection.py
@@ -38,13 +38,67 @@ table_user = connection.table(user_table)
 table_post = connection.table(post_table)
 
 # retreive if there is any data 
-for data in table_user.scan():
-    print(data)
-for data in table_post.scan():
-    print(data)
+# for data in table_user.scan():
+#     print(data)
+# for data in table_post.scan():
+#     print(data)
 # get the data base on the column family and row key
 # ps = table.row(row='fr',columns=['post:language'])
 # print(ps)
+
+
+
+# Initialize a dictionary to store user followers
+user_followers = {}
+# get data from table user
+rows = table_user.scan()
+# Iterate over the rows and store user followers
+for row_key, data in rows:
+    followers_count = int(data[b'user_details:followers_count'])
+    username = data[b'user_details:username'].decode('utf-8')
+    user_followers[username] = followers_count
+# Sort users based on follower count
+sorted_users = sorted(user_followers.items(), key=lambda x: x[1], reverse=True)
+
+# Specify the number of top users you want to retrieve
+num_top_users = 5  
+# Print the top users with the highest number of followers
+print(f"Top {num_top_users} users with the highest number of followers:")
+
+for i, (username, followers_count) in enumerate(sorted_users[:num_top_users]):
+    print(f"{i+1}. User: {username} | Followers: {followers_count}")
+
+
+print("------------------------User engagement------------------------------")
+
+
+# Retrieve post data
+post_rows = table_post.scan()
+
+# Iterate over post rows
+for post_row_key, post_data in post_rows:
+    # Extract relevant data from post data
+    user_id = post_data[b'post_details:user_id'] #.decode('utf-8')
+    reblogs_count = int(post_data[b'post_details:reblogs_count'])
+    favourites_count = int(post_data[b'post_details:favourites_count'])
+    
+
+    # Retrieve user data from user_table
+    user_data = table_user.row(user_id,columns=["user_details:followers_count", "user_details:username"])
+    # print(user_data)
+
+    # Check if user data exists for the post
+    if user_data:
+        followers_count = int(user_data[b'user_details:followers_count'])
+        username = user_data[b'user_details:username'].decode('utf-8')
+        followers_count = 1 if followers_count == 0 else followers_count
+        # Calculate engagement rate
+        engagement_rate = (favourites_count + reblogs_count) / followers_count
+
+        # Print the engagement rate for the user
+        print(f"Engagement rate for user {username}: {round(engagement_rate*100,2)}%")
+    else:
+        print(f"No post data found for user {username}")
 
 # insert into the tables
 for x in f:
@@ -62,7 +116,7 @@ for x in f:
         table_user.put(row_key, {"user_details:" + column:item[3].strip()})
     elif values =='language' or values == 'tag' or values == 'visibility':
         table_post.put(row_key, {"post_details:" + column:item[3].strip()})
-    elif values == 'media' or values == 'favourites_count' or values == 'reblogs_count':
+    elif values == 'media' or values == 'favourites_count' or values == 'reblogs_count' or values == 'user_id':
         table_post.put(row_key, {"post_details:" + column:item[2].strip()})
 
 f.close()
